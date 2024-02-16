be79dd0d26a9a4b7a36eef7b16a4203e02c20a33
Igor <igor@cincatarina.sc.gov.br>
2024-02-16 11:05:04 -0300

    fix: corrige problema com scroll ao abrir dropdown do botão de mudança de tema.

--- src/components/theme/ButtonTheme.tsx
+++ src/components/theme/ButtonTheme.tsx
@@ -15,7 +15,7 @@ export function ButtonTheme() {
   const { changeTheme } = useContext(ThemeContext)
 
   return (
-    <DropdownMenu>
+    <DropdownMenu modal={false}>
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" size="icon">
           <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
